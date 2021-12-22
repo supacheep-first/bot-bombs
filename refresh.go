2050f743253ace2ad3d7e2268e882c90a08818a3
first <first.supacheep@gmail.com>
2021-12-22 11:31:10 +0000

    fix: refresh 1 time

--- refresh.go
+++ refresh.go
@@ -7,11 +7,9 @@ import (
 )
 
 func doRefresh() {
-	for i := 0; i < 3; i++ {
-		robotgo.Sleep(3)
-		robotgo.KeyDown("lctrl")
-		robotgo.KeyTap("f5")
-		robotgo.KeyUp("lctrl")
-	}
+	robotgo.Sleep(3)
+	robotgo.KeyDown("lctrl")
+	robotgo.KeyTap("f5")
+	robotgo.KeyUp("lctrl")
 	fmt.Println("refresh page.")
 }
